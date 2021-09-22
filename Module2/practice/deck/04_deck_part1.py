a4eb2e872957f181263ae681a532b97136fa29c4
paul-izum <63441928+paul-izum@users.noreply.github.com>
2021-09-22 20:38:44 +0300

    Update 04_deck_part1.py

--- Module2/practice/deck/04_deck_part1.py
+++ Module2/practice/deck/04_deck_part1.py
@@ -1,17 +1,47 @@
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    _symbols = {
+        "Spades": "\u2660",
+        "Diamonds": "\u2666",
+        "Hearts": "\u2665",
+        "Clubs": "\u2663"
+    }
 
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def __repr__(self):
+        return f"{self.value}{self._symbols[self.suit]}"
+
+    def to_str(self):
+        return f"{self.value}{self._symbols[self.suit]}"
+
+    def get_suit(self, suit):
+        if suit not in self._symbols:
+            raise KeyError("Такой масти нет")
+        return self._symbols[suit]
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 # Задание: Теперь создадим колоду из 52-ух карт и реализуем все методы
 class Deck:
     def __init__(self):
         # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        suits = ["Hearts", "Diamonds","Clubs", "Spades"]
+        common_deck = list(map(str, range(2, 11))) + ["J", "Q", "K", "A"]
         self.cards = []
+        for suit in suits:
+            for value in common_deck:
+                #cards.append(Card(card, suit))
+                card = Card(value, suit)
+                self.cards.append(card)
+
+
 
     def show(self):
         # Принцип работы данного метода прописан в 00_task_deck.md
-        pass
+        return f"cards[{len(self.cards)}]", ', '.join([card.to_str() for card in self.cards])
 
     def draw(self, x):
         # Принцип работы данного метода прописан в 00_task_deck.md
@@ -26,13 +56,13 @@ class Deck:
 deck = Deck()
 # Выводим колоду в формате указанном в основном задании
 print(deck.show())
-# Тусуем колоду
-deck.shuffle()
-print(deck.show())
-
-# Возьмем 5 карт "в руку"
-hand = deck.draw(5)
-# Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
-print(deck.show())
-# Выводим список карт "в руке"(список hand)
-print(...)
+# # Тусуем колоду
+# deck.shuffle()
+# print(deck.show())
+#
+# # Возьмем 5 карт "в руку"
+# hand = deck.draw(5)
+# # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
+# print(deck.show())
+# # Выводим список карт "в руке"(список hand)
+# print(...)
