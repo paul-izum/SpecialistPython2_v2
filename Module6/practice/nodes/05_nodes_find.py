41dd66a26a06dcefc34733c9d4a1100032bfc04e
paul-izum <63441928+paul-izum@users.noreply.github.com>
2021-09-29 20:15:33 +0300

    Update 05_nodes_find.py

--- Module6/practice/nodes/05_nodes_find.py
+++ Module6/practice/nodes/05_nodes_find.py
@@ -13,7 +13,37 @@ def find_node_by_value(start_node, value):
     Возвращает индекс ноды с заданным значением(value).
     Если нода с указанным значение(value) не найдена, дублируйте поведение методы .index() списка
     """
-    pass
+    i = 0
+    current_node = start_node
+    while current_node:
+        current_node = current_node.next
+        i += 1
+        if current_node == None:
+            print(f"искомое значение {value} не найден")
+            return
+
+        if current_node.value == value:
+            print(i)
+            return
+
+
+def print_node_by_index(start_node, index):
+    """
+    Выводит в терминал значение(value) ноды с заданным индексом(index). Индексация начинается с нуля.
+    Если index = 0, выводим значение ноды start_node
+    Считаем, что index гарантированно > 0
+    """
+    i = 0
+    current_node = start_node
+    while current_node:
+        current_node = current_node.next
+        i += 1
+        if i == index:
+            print(current_node.value)
+            return
+    print(f"искомый элемент {i} не найден")
+
+            # return current_node.value
 
 
 def gen_names_list(size=None):
@@ -27,14 +57,19 @@ def gen_names_list(size=None):
 
 # Дан список из произвольного количества имен
 names = gen_names_list()
-# print(names)
+print(names)
 
 # TODO: скопируйте цепочку нод из предыдущей задачи
-...
+first_node = Node(names[0])
+prev_node = first_node
+for name in names[1:]:
+    next_node = Node(name)
+    prev_node.next = next_node
+    prev_node = next_node
 
 
 # TODO: Передайте первую ноду и искомое значение в функцию find_node_by_value()
 #  напишите реализацию функции find_node_by_value()
-first_node = ...
-value = ...
+
+value = "Василий"
 find_node_by_value(first_node, value)
